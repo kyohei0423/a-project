6131766e0443eb9972f78951129f18998f7cb2f6
kyohei0423 <kyo199333@gmail.com>
2017-05-05 15:43:44 +0900

    グループを保存

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root 'messages#index'
   resource :user, only: [:edit, :update]
-  resource :groups, only: [:new]
+  resource :groups, only: [:new, :create]
 end
