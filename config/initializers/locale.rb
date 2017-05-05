c1ae099cc6b2abb551a4ac1e5185db2980c9826b
kyohei0423 <kyo199333@gmail.com>
2017-05-05 15:44:33 +0900

    エラーメッセージの日本語化

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,6 @@
 module ChatSpace
   class Application < Rails::Application
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
