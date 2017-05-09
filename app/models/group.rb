f93156fd6b97e45a9a8eba3b8232541ec7aa81e7
kyohei0423 <kyo199333@gmail.com>
2017-05-09 09:57:41 +0900

    サイドバーに表示するメッセージの修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -6,10 +6,8 @@ class Group < ApplicationRecord
   validates :name, presence: true
 
   def show_last_message
-    if messages.last.content.present?
-      massages.last.content
-    elsif messages.last.image.present?
-      '画像が投稿されています。'
+    if (last_message = messages.last).present?
+      last_message.content? ? last_message.content : '画像が投稿されています'
     else
       'まだメッセージはありません。'
     end
